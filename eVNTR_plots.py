f9e54cc3d7457b50702838a160ceaa03f7d5decd
Mehrdad Bakhtiari <mehrdad.baxtiari@gmail.com>
2019-06-11 17:20:52 -0700

    add definition for all required plots

--- eVNTR_plots.py
+++ eVNTR_plots.py
@@ -8,6 +8,9 @@ from math import log10
 
 from advntr.models import load_unique_vntrs_data
 
+from run_anova import load_individual_genotypes
+
+
 vntr_models_dir = '/home/mehrdad/workspace/adVNTR/vntr_data/hg38_selected_VNTRs_Illumina.db'
 ref_vntrs = load_unique_vntrs_data(vntr_models_dir)
 reference_vntrs = {}
@@ -75,22 +78,110 @@ def plot_variant_caviar_scores(vntr_id, tissue_name):
     plt.close()
 
 
-def plot_variant_pvalues():
+def plot_variant_pvalues(vntr_id, tissue_name):
+    pvalue_file = 'pvalues/%s/%s/pvalues.txt' % (tissue_name, vntr_id)
+    with open(pvalue_file) as infile:
+        lines = infile.readlines()
+    lines = [line.strip() for line in lines if line.strip() != '']
+    y = []
+    x = []
+    vntr_x = 104629254 # I should read from hg19 :((
+    vntr_y = 0
+    for line in lines:
+        snp, p = line.split()
+        if '%s' % vntr_id in snp:
+            vntr_y = -log10(float(p))
+        else:
+            y.append(-log10(float(p)))
+            x.append(float(snp.split('_')[1]))
+    if vntr_id != 111235:
+        vntr_x = sum(x) / len(x)
+
+    gene_name = reference_vntrs[vntr_id].gene_name
+
     fig = plt.figure(figsize=(6, 2))
     ax = fig.add_subplot(111)
-
-    gene_name = 'AS3MT'
-
-    data = [0.0001, 0.000005, 0.001, 0.01, 0.001, 0.0001, 0.001, 0.0001]
-    data = [-log10(e) for e in data]
-    x = [i for i in range(len(data))]
-    ax.scatter(x, data, marker='.', c='gray')
-    vntr_x = 4.5
-    vntr_p_value = -log10(0.000000000001)
-    ax.scatter(vntr_x, vntr_p_value, marker='*', c='r')
+    ax.scatter(x, y, marker='.', c='gray')
+    ax.scatter(vntr_x, vntr_y, marker='*', c='r')
     # ax.set_xlabel('Genomic location')
     ax.set_ylabel('-log10 P-value')
-    fig.savefig('pvalues_%s.pdf' % gene_name)
+    fig.savefig('pvalues_%s_%s.pdf' % (gene_name, tissue_name))
+    plt.cla()
+    plt.clf()
+    plt.close()
+
+
+def plot_allele_count_distribution():
+    genotypes = load_individual_genotypes(reference_vntrs)
+    genotyped_vntr_ids = {_id: set() for _id in range(1000000)}
+
+    for individual_id, result_map in genotypes.items():
+        for genotyped_vntr, avg_length in result_map.items():
+            if avg_length == 'None' or avg_length is None:
+                continue
+            genotyped_vntr_ids[genotyped_vntr].add(avg_length)
+            #    print(genotyped_vntr_ids)
+    vntr_genotypes = {_vntr_id: len(lengths) for _vntr_id, lengths in genotyped_vntr_ids.items() if len(lengths) != 0}
+
+
+    plt.hist(vntr_genotypes.values())
+    plt.xlabel('Number of alleles')
+    plt.ylabel('Number of VNTRs')
+    plt.savefig('VNTR_genotype_allele_count.pdf')
+    plt.cla()
+    plt.clf()
+    plt.close()
+
+
+def plot_evntrs_and_number_of_tissues():
+    plt.xlabel('Shared across number of tissues')
+    plt.ylabel('Number of VNTR-eQTLs')
+    plt.savefig('eVNTR_uniqueness.pdf')
+    plt.cla()
+    plt.clf()
+    plt.close()
+
+
+def plot_evntrs_per_tissue():
+    plt.xlabel('Tissue name')
+    plt.ylabel('Number of VNTR-eQTLs')
+    # plt.savefig('eVNTR_uniqueness.pdf')
+    plt.cla()
+    plt.clf()
+    plt.close()
+
+
+def plot_difference_from_reference():
+    plt.xlabel('Number of VNTRs')
+    plt.ylabel('Base-pairs difference from reference')
+    # plt.savefig('eVNTR_uniqueness.pdf')
+    plt.cla()
+    plt.clf()
+    plt.close()
+
+def plot_tissue_correlations():
+    #matshow or annotated heatmaps in matplotlib
+    # or hexbin
+    pass
+
+
+def plot_vntr_location_effect():
+    # y: pvalue
+    # x: distance to transcription start site, gene start, first exon, ...
+    pass
+
+
+def plot_significant_vntrs_and_tissues():
+    # show which tissues are affected for a list of 20-30 VNTRs (end of page11 in Melissa's paper)
+    pass
+
+
+#GWAS result? correlation with traits? like hight
+
+def plot_vntr_polymorphic_rate_based_on_annotation():
+    plt.xlabel('Annotation')
+    plt.ylabel('Rate of polymorphic VNTRs')
+    plt.savefig('polymorphic_vntrs.pdf')
     plt.cla()
     plt.clf()
     plt.close()
@@ -98,7 +189,9 @@ def plot_variant_pvalues():
 
 if __name__ == '__main__':
     # plot_expression_genotype_correlation()
-    # plot_variant_pvalues()
+    plot_variant_pvalues(101865, 'Heart')
+    plot_variant_pvalues(331737, 'Heart')
     plot_variant_caviar_scores(111235, 'Esophagus')
     plot_variant_caviar_scores(331737, 'Esophagus')
 
+    plot_allele_count_distribution()
