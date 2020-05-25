781042f6cde5c9f4e73ef757003ed21598fab3fe
Mehrdad Bakhtiari <mehrdad.baxtiari@gmail.com>
2020-05-24 17:02:37 -0700

    fix extract_expression_by_tissue directories

--- extract_expression_by_tissue.py
+++ extract_expression_by_tissue.py
@@ -1,7 +1,13 @@
 rna_table_file = '../Sra_table_RNA-Seq_only'
 rpkm_file = '../files/phe000020.v1.GTEx_RNAseq.expression-data-matrixfmt.c1/GTEx_Data_20160115_v7_RNAseq_RNASeQCv1.1.8_gene_rpkm.gct'
-
 expression_by_tissue_dir = '../Expression_by_Subtissue/'
+
+import sys
+if (sys.argv) >= 4:
+    rna_table_file = sys.argv[1]
+    rpkm_file = sys.argv[2]
+    expression_by_tissue_dir = sys.argv[3]
+
 protein_id_to_gene = {}
 
 
@@ -13,6 +19,9 @@ def write_expression_for_tissue(tissue_name, data):
     output_file = expression_by_tissue_dir + tissue_name + '.rpkm'
     rows = len(data.keys())
     columns = len(data[data.keys()[0]][tissue_name].keys())
+    dirname = os.path.dirname(output_file)
+    if not os.path.exists(dirname):
+        os.makedirs(dirname)
     with open(output_file, 'w') as outfile:
         outfile.write('%s\t%s\n' % (rows, columns))
         outfile.write('Name\tDescription\t')
